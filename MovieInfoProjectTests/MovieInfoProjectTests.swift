ea52b77dc0019ad7c129e179e851fe953e62af59
Nick Rigano <nrigano@gmail.com>
2017-01-30 14:02:23 -0500

    Initial Commit

---
+++ MovieInfoProjectTests/MovieInfoProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  MovieInfoProjectTests.swift
+//  MovieInfoProjectTests
+//
+//  Created by Nick on 1/30/17.
+//  Copyright © 2017 Nick. All rights reserved.
+//
+
+import XCTest
+@testable import MovieInfoProject
+
+class MovieInfoProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
